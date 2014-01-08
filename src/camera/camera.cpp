239b9e8bce04d742bd6398b451bf2e90b21dbff5
Josh Hofing <joshhofing@gmail.com>
2014-01-07 19:24:56 -0500

    Add Camera class, currently doesn't do anything, will in the FUUUUTURE

---
+++ src/camera/camera.cpp
@@ -0,0 +1,8 @@
+#include "camera.h"
+
+Camera::Camera() {
+}
+
+Mat Camera::getFrame() {
+  return NULL; //TODO: Make this not shit
+}
---
+++ src/camera/camera.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <opencv2/core/core.hpp>
+
+#define AXIS_IP "10.6.94.12"
+
+class Camera {
+public:
+  Camera();       // Creates the camera, detecting whether or not
+                  // there is a camera attached that is valid for VideoCapture
+                  // Otherwise, it will use an AXIS camera at the IP Address
+                  // defined by AXIS_IP
+  Mat getFrame(); // Returns the current camera frame
+private:
+  
+};
