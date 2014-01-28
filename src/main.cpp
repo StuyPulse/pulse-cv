99e8fb74e7cf745d6c103576ede1a4a607f5251d
Josh Hofing <joshhofing@gmail.com>
2014-01-28 15:29:28 -0500

    Whoops, remove diff marker from main

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-<<<<<<< HEAD
-//#include "camera/camera.h"
-//#include "camera/camera.cpp"
-=======
 #include "camera/camera.h"
->>>>>>> Add camera input both for webcam and AXIS cam. not the most efficient, but it works
 
 using namespace std;
 using namespace cv;
