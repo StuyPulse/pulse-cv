152a323dca0d137700f7f812b2c248e3811a5e6f
Josh Hofing <joshhofing@gmail.com>
2014-02-04 19:02:48 -0500

    Performance tuning, broken line detection algo

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "camera/camera.h"
 #include "net/netsend.h"
+#include <chrono>
 
 using namespace std;
 using namespace cv;
@@ -17,6 +18,8 @@ using namespace cv;
 #define I_DONT_KNOW 0
 #define FRAME_SEEN 1
 #define FRAME_UNSEEN 2
+
+//#define printf(...) [>...<]
 
 void printVec(Vec4i vec) {
   printf("%d, %d to %d, %d\n", vec[0], vec[1], vec[2], vec[3]);
@@ -39,6 +42,65 @@ vector<Vec4i> exterminate_in_range(vector<Vec4i> pts, Vec4i mid, int range) {
   }  
 
   return results;
+
+}
+
+
+vector<Vec4i> detect_lines(Mat* image) {
+  const double err = 0.000000001f;
+  vector<Vec4i> result = vector<Vec4i>();
+
+  for (int i = 0; i < image->rows - 2; i++) {
+    bool tracking = false;
+    const double* row = image->ptr<double>(i);
+    Vec4i curr = Vec4i();
+    bool should_print = false;
+
+    for (int j = 0; j < image->cols; j++) {
+      double pix = image->at<double>(i,j);
+      if (pix != 0) should_print = true;
+      if (pix != 0) {//>= err) {
+        // We've hit the start of the line
+        if (!tracking) {
+          tracking = true;
+          // Save our starting point
+          curr[0] = j;
+          curr[1] = i;
+        }
+      } else if (tracking) { // We've hit the end of the line.
+        tracking = false;
+        // Save our ending point, push onto our list of lines
+        curr[2] = j;
+        curr[3] = i;
+        result.push_back(curr);
+        printf("Reached end of line naturally\n");
+        printVec(curr);
+        curr = Vec4i();
+        break;
+      }
+    }
+    // We've hit the end, finish our line if we've started one
+    //printf("Before eol\n");
+    if (tracking) {
+        tracking = false;
+        // Save our ending point, push onto our list of lines
+        curr[2] = image->cols;
+        curr[3] = i;
+        printf("Reached end of line because image edge\n");
+        result.push_back(curr);
+        curr = Vec4i();
+    }
+    if (should_print) {
+      for (int j = 0; j < image->cols; j++) {
+        printf("%d", (row[j]==0?0:1));
+      }
+      printf("\n");
+    }
+    //printf("after eol %d of %d\n", i, image.rows);
+  }
+
+  printf("End of detect_lines\n");
+  return result;
 
 }
 
@@ -90,13 +152,17 @@ int main() {
   n.start_server();
   sleep(1);
   n.stop_server();
-  while(1);
+
+  std::chrono::steady_clock::time_point start, end;
+  double totalTime = 0;
+  int iterations = 0;
 
   Camera cam;
   namedWindow("frame");
   bool running = true;
   char buffer[64];
   while (running) {
+
     Mat frame = cam.getFrame();
     //cvtColor(frame, frame, CV_BGR2HSV);
     inRange(frame, Scalar(220, 175, 200), Scalar(255,255,255), frame);
@@ -105,7 +171,14 @@ int main() {
     morphologyEx(frame, frame, MORPH_CLOSE, kernel);
     vector<Vec4i> lines;
     vector<Vec4i> horizontals = vector<Vec4i>();
-    HoughLinesP(frame, lines, 1, CV_PI/180, 50, 30, 10);
+    start = std::chrono::steady_clock::now();
+    HoughLinesP(frame, lines, 3, CV_PI/5, 50, 30, 10);
+    //lines = detect_lines(&frame);
+    end = std::chrono::steady_clock::now();
+    iterations++;
+    using namespace std::chrono;
+    totalTime += duration<int, std::milli>(duration_cast<duration<int, std::milli> >(end-start)).count();
+    printf("Avg time: %f f/s\n", totalTime/iterations);
     cvtColor(frame, frame, CV_GRAY2BGR);
     for (int i = 0; i < lines.size(); i++) {
       double yd = (lines[i][3] - lines[i][2]);
