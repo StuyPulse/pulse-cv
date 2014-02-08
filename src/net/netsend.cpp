5faa6da02020ddaec289752ef89f45f530444b11
Josh Hofing <joshhofing@gmail.com>
2014-02-08 12:05:57 -0500

    When a client disconnects, start listening anew.

--- src/net/netsend.cpp
+++ src/net/netsend.cpp
@@ -20,44 +20,51 @@ int NetSend::stop_server() {
 }
 
 void* NetSend::init_server(void* threadarg) {
-  int socket_id, socket_client;
-  char buffer[256];
-
-  struct sockaddr_in server, client;
-  socklen_t socket_length;
-
-  socket_id = socket(AF_INET, SOCK_STREAM, 0);
-  printf("socket id: %d\n", socket_id);
-   
-  // IPv4, yo
-  server.sin_family = AF_INET;
-  // Listen to everything coming in.
-  server.sin_addr.s_addr = INADDR_ANY;
-  // Select the coolest port
-  server.sin_port = htons(PORT);
-
-  // Bind the socket
-  int res = bind(socket_id, (struct sockaddr *)&server, sizeof(server));
-  if (res < 0) {printf("Binding failed.") ; }
-  //Check if "linger" code is necessary
-
-  // Listen to the socket
-  res = listen(socket_id, 0);
-  if (res < 0) {printf("Listening failed.") ; }
-
-  printf("Waiting for connection\n");
-  socklen_t l = sizeof(client);
-  //Do we need a different variable than server?
-  res = accept(socket_id, (struct sockaddr *)&client, &l);
-  if (res < 0) {printf("Accepting failed.") ; }
-  printf("Connected!\n");
   while (1) {
+    int socket_id, socket_client;
+    char buffer[256];
 
-    //printf("Sending data: %d\n", send_value);
-    int wrval = write(res, threadarg, sizeof(int));
-    if (wrval == -1) printf("Failed write: %s\n", strerror(errno));
+    struct sockaddr_in server, client;
+    socklen_t socket_length;
 
-    read(res, buffer, sizeof(buffer));
+    socket_id = socket(AF_INET, SOCK_STREAM, 0);
+    printf("socket id: %d\n", socket_id);
+
+    // IPv4, yo
+    server.sin_family = AF_INET;
+    // Listen to everything coming in.
+    server.sin_addr.s_addr = INADDR_ANY;
+    // Select the coolest port
+    server.sin_port = htons(PORT);
+
+    // Bind the socket
+    int res = bind(socket_id, (struct sockaddr *)&server, sizeof(server));
+    if (res < 0) {printf("Binding failed.\n") ; }
+    //Check if "linger" code is necessary
+
+    // Listen to the socket
+    res = listen(socket_id, 0);
+    if (res < 0) {printf("Listening failed.\b") ; }
+
+    printf("Waiting for connection\n");
+    socklen_t l = sizeof(client);
+    //Do we need a different variable than server?
+    res = accept(socket_id, (struct sockaddr *)&client, &l);
+    if (res < 0) {printf("Accepting failed.\n") ; }
+    printf("Connected!\n");
+    bool running = true;
+    while (running) {
+
+      //printf("Sending data: %d\n", send_value);
+      int wrval = write(res, threadarg, sizeof(int));
+      if (wrval == -1) {
+        printf("Failed write: %s %d\n", strerror(errno), errno);
+        running = false;
+        close(socket_id);
+      } else {
+        read(res, buffer, sizeof(buffer));
+      }
+    }
   }
   pthread_exit(NULL);
 }
