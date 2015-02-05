4693d9319f1b3cf5d0339daefe629427d394b423
James Wang <jameswang9909@hotmail.com>
2015-02-04 23:19:02 -0500

    Fix PolygonDetect to detect quadrilaterals

--- 2014_2015/Practice/Java/PolygonDetect.java
+++ 2014_2015/Practice/Java/PolygonDetect.java
@@ -10,6 +10,8 @@ import org.opencv.core.MatOfPoint2f;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
 
+import com.atul.JavaOpenCV.Imshow;
+
 public class PolygonDetect {
 
 	public static int polyDetect(Mat src , int poly) {
@@ -33,19 +35,14 @@ public class PolygonDetect {
 			contour.get(i).convertTo(MOP2f1, CvType.CV_32FC2);
 			System.out.println("MOP2f1 = " + MOP2f1);
 			System.out.println("Checkpoint 2");
-			Imgproc.approxPolyDP(MOP2f1 , MOP2f1, 0.02 , true);
-			System.out.println("Checkpoint 3");
+			Imgproc.approxPolyDP(MOP2f1 , MOP2f1, 1 , true); //Changed epsilon from 0.02 to 1 (Quad detection works).
 			MOP2f1.convertTo(contourMOP, CvType.CV_32SC2);
-			System.out.println("Checkpoint 4");
 			System.out.println("Size of contourMOP: " + contourMOP.size());
 			if(Math.abs(Imgproc.contourArea(contour.get(i))) < 10 || Imgproc.isContourConvex(contourMOP)) {
 				System.out.println("Checkpoint 5");
-				//continue;
+				//break;
 			}
-			System.out.println("Checkpoint 6");
 			List<Point> contourList = contourMOP.toList();
-			System.out.println("Checkpoint 7");
-			System.out.println("Size of contourList: " + contourList.size());
 			if (contourList.size() == poly) {
 				System.out.println("Checkpoint 7.5");
 				polyFound++;
@@ -53,12 +50,14 @@ public class PolygonDetect {
 			System.out.println("Checkpoint 8");
 		}
 		System.out.println("Polygons detected: " + polyFound);
+		Imshow im = new Imshow("Preview"); 
+		im.showImage(gray); //Show image for reference
 		return polyFound;
 	}
 	
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		Mat img = Highgui.imread("/home/sublimau5/eclipse/workspaces/CV-JAVA/src/QUAD2.png");
+		Mat img = Highgui.imread("/home/james/Dev/pulse-cv/2014_2015/Practice/QuadrilateralDetect/QUAD5.png");
 		polyDetect(img , 4);
 	}
 }
