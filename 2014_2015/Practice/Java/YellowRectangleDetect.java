5b5317ad63247984bbf089af1b3bc004f7dac33d
James Wang <jameswang9909@hotmail.com>
2015-02-14 13:59:35 -0500

    Fix YellowRectangleDetect to detect the two largest quadrilaterals

--- 2014_2015/Practice/Java/YellowRectangleDetect.java
+++ 2014_2015/Practice/Java/YellowRectangleDetect.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
@@ -29,7 +31,8 @@ public class YellowRectangleDetect {
 
 		Mat tracked = frame.clone();
 		Mat edges = new Mat();
-		Rect rect = new Rect(0,0,0,0);
+		Rect firstRect = new Rect(0,0,0,0);
+		Rect secondRect = new Rect(0,0,0,0);
 		MatOfPoint2f MOP2f = new MatOfPoint2f();
 		ArrayList<MatOfPoint> contour = new ArrayList<MatOfPoint>();
 
@@ -37,15 +40,13 @@ public class YellowRectangleDetect {
 		Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2HSV);
 
 		// Working yellow ranges
-		Core.inRange(frame, new Scalar(23 , 50 , 50) , new Scalar(35 , 255 , 255), frame);
+		Core.inRange(frame, new Scalar(15 , 100 , 100) , new Scalar(35 , 255 , 255), frame);
 
 		// Smooth edges and find contours
-		
-		//Imgproc.Canny(frame, edges, 0, 100);
+		Imgproc.Canny(frame, edges, 0, 100);
 		Imgproc.GaussianBlur(frame, edges, new Size(3,3), 1.5, 1.5);
 
 		// Sequence of erode and dilate to remove weird noise
-		// Upon using Canny and the dilation sequences, things break
 		Imgproc.erode(edges , edges , Imgproc.getStructuringElement(Imgproc.MORPH_RECT , new Size(3,3)));
 		Imgproc.dilate(edges , edges , Imgproc.getStructuringElement(Imgproc.MORPH_RECT , new Size(3,3)));
 		Imgproc.dilate(edges , edges , Imgproc.getStructuringElement(Imgproc.MORPH_RECT , new Size(3,3)));
@@ -54,8 +55,10 @@ public class YellowRectangleDetect {
 		Imgproc.threshold(edges, edges, 127, 255, Imgproc.THRESH_BINARY);
 		Imgproc.findContours(edges, contour, new Mat() , Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 
-		double currArea = -1.0; // Current largest area
+		double currLargestArea = -1.0;
+		double currSecondArea = -2.0;
 		int max_polygon_index = -1; // The index of the largest polygon
+		int second_polygon_index = -2;
 
 		List<Point> contourList = null;
 
@@ -67,18 +70,64 @@ public class YellowRectangleDetect {
 			Imgproc.approxPolyDP(MOP2f, MOP2f, 2 , true);
 			// Convert into a list
 			contourList = MOP2f.toList();
-
 		}
 
-		// Acquire only the largest quadrilateral
-		for (int i = 1 ; i < contour.size() ; i++) {
+		double area = -1.0;
+
+		/* for (int i = 1 ; i < contour.size() ; i++) {
+
+			System.out.printf("%f , %f\n" , currLargestArea , currSecondArea);
 			if (contourList.size() == 4) {
-				if (contour.get(i).width() * contour.get(i).height() > currArea) {
+				area = contour.get(i).width() * contour.get(i).height();
+
+				if (area >= currLargestArea) {
 					max_polygon_index = i;
-					currArea = contour.get(i).width() * contour.get(i).height();
-				}
-			}	
+					currLargestArea = area;
+				} else if (area < currLargestArea && area >= currSecondArea) {
+					System.out.println(area < currLargestArea);
+					System.out.println(area);
+					System.out.println(area > currSecondArea);
+					second_polygon_index = i;
+					currSecondArea = area;
+				}		
+			}
+		} */
+
+		Collections.sort(contour, new Comparator<MatOfPoint>() {
+			public int compare(MatOfPoint first, MatOfPoint second) {
+
+				double firstArea = first.width() * first.height();
+				double secondArea = second.width() * second.height();
+
+				double diff = secondArea - firstArea;
+
+				return (int) diff;
+
+			}
 		}
+		);
+		
+		if(contour.size() >= 2) {
+
+			MatOfPoint pts = new MatOfPoint(contour.get(1));
+			MatOfPoint pts2 = new MatOfPoint(contour.get(2));
+
+			firstRect = Imgproc.boundingRect(pts);	
+			secondRect = Imgproc.boundingRect(pts2);
+
+			// We know that the origin (0,0) is at the top left
+			Point firstTopLeft = new Point(firstRect.x, firstRect.y);
+			Point secondTopLeft = new Point(secondRect.x, secondRect.y);
+
+			// y coordinate has to be rect.y + rect.height because subtracting brings the boundingRect up?
+			Point firstLowerRight = new Point(firstRect.x+firstRect.width, firstRect.y+firstRect.height);
+			Point secondLowerRight = new Point(secondRect.x+secondRect.width, secondRect.y+secondRect.height);
+
+			Core.rectangle(tracked, firstTopLeft, firstLowerRight, new Scalar(0, 255, 0), 2);
+			Core.rectangle(tracked, secondTopLeft, secondLowerRight, new Scalar(255, 0, 0), 2);
+
+		}
+
 		// IRRELEVANT
 		// Use Iterator to go through the contours 
 		/*
@@ -92,16 +141,6 @@ public class YellowRectangleDetect {
 				iterator.remove();
 			}
 		} */
-
-		MatOfPoint pts = new MatOfPoint(MOP2f.toArray());
-		rect = Imgproc.boundingRect(pts);
-
-		// We know that the origin (0,0) is at the top left
-		Point topLeft = new Point(rect.x, rect.y);
-		// y coordinate has to be rect.y + rect.height because subtracting brings the boundingRect up?
-		Point lowerRight = new Point(rect.x+rect.width, rect.y+rect.height);
-		Core.rectangle(tracked, topLeft, lowerRight, new Scalar(30, 100, 255), 2);
-
 		return tracked; 
 	}
 
@@ -115,38 +154,43 @@ public class YellowRectangleDetect {
 
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);		
-		VideoCapture cap = new VideoCapture(0);
+		//VideoCapture cap = new VideoCapture(0);
 		Mat src = new Mat();
-
+		Mat dst = new Mat();
+		/*
 		if (!cap.isOpened()) {
 			System.out.println("VideoCapture device not detected.");
 			System.exit(0);
 		}
-
+		 */
 		Imshow cam = new Imshow("Feed");
+		Imshow cam2 = new Imshow("Binary");
 
 		// Live feed
+		/*
 		long starttime = System.currentTimeMillis();
 		for (;;) {
 			if (cap.grab() && System.currentTimeMillis() - starttime > 150) {
 				starttime = System.currentTimeMillis();
 				try {
 					if (cap.retrieve(src)) {
+						dst = src.clone();
+						dst = bin(dst);
 						src = yellowRectangleDetect(src);
 						cam.showImage(src);
+						cam2.showImage(dst);
 					}
 
 				} catch(Exception e) {
 
 				}
 			}
-		} 
+		} */
 
 		// Single image test (note that single image works better than live feed)
-		/*
 		Mat img = Highgui.imread("/home/james/Dev/pulse-cv/2014_2015/Practice/Java/YELLOW.png");
 		img = yellowRectangleDetect(img);
 		cam.showImage(img);
-		*/
+
 	}
 }
