03e0156a023b7273b00497ced130045e2a1352c9
James Wang <jameswang9909@hotmail.com>
2015-12-18 22:31:05 -0500

    Install OpenCV from official git repository, not sourceforge

--- install_opencv.sh
+++ install_opencv.sh
@@ -1,41 +1,47 @@
 #!/bin/bash
 # Script to install OpenCV libraries
 
-set -e # Abort if any command returns a non-zero exit code
+RED="\033[1;31m"
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+RESET="\033[m"
 
-echo -n "Download latest version? [y/n] "
-read ans
-if [[ $ans =~ ^[Yy]$ ]]; then
-    version="$(wget -q -O - http://sourceforge.net/projects/opencvlibrary/files/opencv-unix | egrep -m1 -o '\"[0-9](\.[0-9]+)+' | cut -c2-)"
-else
-    echo -n "What version would you like to install?: "
-    read ans
-    version=$ans
+if [ ! -d "$HOME/OpenCV" ]; then
+    mkdir "$HOME/OpenCV"
 fi
-echo "Installing OpenCV $version"
+cd "$HOME/OpenCV"
 
-if [ ! -d $HOME/OpenCV ]; then
-    mkdir $HOME/OpenCV
-fi
-cd $HOME/OpenCV
-
-echo "Removing any pre-installed ffmpeg and x264"
-sudo apt-get -qq remove ffmpeg x264 libx264-dev
-echo "Installing Dependenices"
-sudo apt-get -y -qq install libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils ffmpeg unzip
+sudo apt-get -qq update
+printf "${GREEN}Installing dependenices...${RESET}\n"
+sudo apt-get -y -qq install git libopencv-dev build-essential checkinstall cmake pkg-config yasm libjpeg-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils unzip ant
 sudo -v
 
-echo "Downloading OpenCV $version"
-wget -O "OpenCV-$version.zip" "http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/opencv-$version.zip/download"
+if [ ! -d "$HOME/OpenCV/opencv" ]; then
+    printf "${YELLOW}Cloning OpenCV...${RESET}\n"
+    git clone https://github.com/itseez/opencv
+fi
 
-echo "Installing OpenCV $version"
-unzip "OpenCV-$version.zip"
+cd opencv
 
-cd "opencv-$version"
-mkdir build
+printf "${YELLOW}What version would you like to install?${RESET} "
+read version
+if [[ $(git rev-parse --verify "$version") =~ fatal ]]; then
+    printf "${RED}Version does not exist!${RESET}\n"
+    exit 1
+fi
+git checkout "$version"
+if [ ! -d "$HOME/OpenCV/opencv/build" ]; then
+    mkdir build
+fi
 cd build
+
+# Set JAVA_HOME so that OpenCV will build with java
+if [ $JAVA_HOME == "" ]; then
+    export JAVA_HOME=$(update-alternatives --query java | grep 'Value: ' | grep -o '/.*oracle')
+fi
+make clean
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
-make -j4
+make -j4 # 4 cores
 
 sudo make install
 sudo -v
@@ -44,10 +50,10 @@ sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
 
 # OpenCV version 3 and up doesn't have highgui, rendering the following lib useless
-if [[ $version =~ ^3 ]]; then
-    echo "Installing external ImShow library for OpenCV Java"
+if [[ $version =~ ^3 && ! -d "$HOME/OpenCV/ImShow-Java-OpenCV" ]]; then
+    printf "${YELLOW}Installing external ImShow library for OpenCV Java${RESET}\n"
     git clone https://github.com/master-atul/ImShow-Java-OpenCV ~/OpenCV
 fi
 
-echo "OpenCV $VERSION is now installed!"
-echo "Run 'g++ <file>.cpp -o <file> `pkg-config --libs --cflags opencv`' to compile"
+printf "${GREEN}OpenCV $version is now installed!${RESET}\n"
+echo "For c++ files, run 'g++ <file>.cpp -o <file> \`pkg-config --libs --cflags opencv\`' to compile"
